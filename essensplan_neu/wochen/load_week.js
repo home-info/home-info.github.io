d3891e96231c82be520018e08088f9906098fe52
Hannes Kainz <hannes@mac.speedport.ip>
2024-12-29 23:35:45 +0100

    Update load_week.js

--- essensplan_neu/wochen/load_week.js
+++ essensplan_neu/wochen/load_week.js
@@ -22,5 +22,34 @@ function loadHTML(filePath, targetElementId) {
         });
 }
 
+// Funktion zur Berechnung der Kalenderwoche (Freitag-Donnerstag)
+function getCustomWeek() {
+    const today = new Date();
+    const dayOfWeek = today.getDay(); // Sonntag = 0, Montag = 1, ..., Samstag = 6
+
+    // Anpassung für Freitag-Donnerstag-Woche
+    const adjustment = (dayOfWeek >= 5) ? 0 : (dayOfWeek + 2);
+    const referenceDate = new Date(today);
+    referenceDate.setDate(today.getDate() - adjustment);
+
+    // Berechnung der ISO-Woche
+    const jan4 = new Date(referenceDate.getFullYear(), 0, 4);
+    const firstWeekStart = new Date(jan4);
+    firstWeekStart.setDate(jan4.getDate() - (jan4.getDay() || 7) + 1);
+
+    const weekNumber = Math.ceil(((referenceDate - firstWeekStart) / (7 * 24 * 60 * 60 * 1000)) + 1);
+    return weekNumber;
+}
+
+// Dynamisch den Link erzeugen
+function generateLink() {
+    const weekNumber = getCustomWeek();
+    const baseUrl = "./woche-";
+    const link = <a href="${baseUrl}${weekNumber}.html"></a>;
+    document.getElementById("speiseplan-link2").innerHTML = link;
+}
+
+generateLink;
+
 // Beispiel: HTML-Datei laden und in ein Div mit der ID "content" einfügen
-loadHTML('./woche-03.html', 'content');
+loadHTML(link, 'content');
