5ec9f27bb8e7abe15dbe72dde79909f5d9caea7d
Hannes Kainz <hannes@Hannes-MacBook.local>
2025-04-06 18:49:51 +0200

    Update script.js

--- anthro/script.js
+++ anthro/script.js
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", () => {
       upcomingEvents.forEach(event => {
         const eventElement = document.createElement('div');
         eventElement.classList.add(
-          'border-l-2',
+          'border-l-4',
           'border-[var(--akzent)]',
           'mb-4',
           'shadow-md',
