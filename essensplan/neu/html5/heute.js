0074c23a62b0542d54779506745eb000b8765559
home-info <rundablage@email-postfach.info>
2025-12-23 16:10:46 +0100

    s

--- essensplan/neu/html5/heute.js
+++
@@ -1,8 +0,0 @@
-var d  = new Date();
-var tt = d.getDate();
-var m = ("0" + (d.getMonth() + 1)).slice(-2)
-var yy = d.getFullYear();
-var h = String(yy) + String(m) + String(tt)
-
-
-document.writeln("<a href='#" + h + "' class='jumplink pic'>")
